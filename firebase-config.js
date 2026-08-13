6715c8ac32122d7da615cc7da6c19ed9273de3c0
waka0338-lgtm <waka0338@gmail.com>
2026-08-13 11:16:01 +0900

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+vexport const firebaseConfig = {
+  apiKey: "ここに貼り付け",
+  authDomain: "ここに貼り付け",
+  projectId: "ここに貼り付け",
+  storageBucket: "ここに貼り付け",
+  messagingSenderId: "ここに貼り付け",
+  appId: "ここに貼り付け"
+};
